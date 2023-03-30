72c1a5b4d11b27ce9e7efce6096ec5f98a8d2e89
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2023-03-30 06:37:02 +0530

    frontend improvements

--- AQ-FRONTEND/src/Layout/Footer.js
+++ AQ-FRONTEND/src/Layout/Footer.js
@@ -1,8 +1,18 @@
-const AquaFooter = () =>{
-return(
-    <>
-    
-    </>
-)
-}
-export default AquaFooter
+import Card from "react-bootstrap/Card";
+
+const AquaFooter = () => {
+    let Year = new Date().getFullYear()
+  return (
+    <Card body className="aqua-footer fixed-bottom">
+      This is some text within a card body. hello test.
+      <Card className="bg-primary">
+        <Card.Body>
+         
+          <h4 className="text-white text-center">Aquakart © {Year}</h4>
+        </Card.Body>
+      </Card>
+    </Card>
+  );
+};
+
+export default AquaFooter;
