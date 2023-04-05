88ee2eb731c0d4d18779a5fdeddcb0dd50d15999
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2023-04-05 23:30:58 +0530

    new backend test

--- AQ-TNB/models/order.js
+++ AQ-TNB/models/order.js
@@ -1,35 +1,111 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const { ObjectId } = mongoose.Schema;
 
-const orderSchema = new mongoose.Schema(
-  {
-    products: [
-      {
-        product: {
-          type: ObjectId,
-          ref: "Product",
-        },
-        count: Number,
-        color: String,
-      },
-    ],
-    paymentIntent: {},
-    orderStatus: {
+// shippingInfo{}
+// user,
+// paymentInfo{}
+// taxAmount
+// ShippingAmount
+// totalAmount
+// orderStatus
+// deliveredAt
+// createdAt
+// -------------
+// orderItems: [ {} ]
+// - name
+// - quantity
+// - image[0]
+// - price
+// - product
+
+const orderSchema = new mongoose.Schema({
+  shippingInfo: {
+    address: {
       type: String,
-      default: "Not Processed",
-      enum: [
-        "Not Processed",
-        "Cash On Delivery",
-        "processing",
-        "Dispatched",
-        "Cancelled",
-        "Completed",
-      ],
+      required: true,
     },
-    orderdBy: { type: ObjectId, ref: "User" },
+    city: {
+      type: String,
+      required: true,
+    },
+    phoneNo: {
+      type: String,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      // required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
   },
-  { timestamps: true }
-);
+  user: {
+    type: mongoose.Schema.ObjectId, //mongoose.Schema.Types.ObjectId
+    ref: "User",
+    required: true,
+  },
+  orderItems: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      image: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      product: {
+        type: mongoose.Schema.ObjectId, //mongoose.Schema.Types.ObjectId
+        ref: "Product",
+        required: true,
+      },
+    },
+  ],
+  paymentInfo: {
+    id: {
+      type: String,
+    },
+  },
+  taxAmount: {
+    type: Number,
+    required: true,
+  },
+  shippingAmount: {
+    type: Number,
+    required: true,
+  },
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+  orderStatus: {
+    type: String,
+    required: true,
+    default: "processing",
+  },
+  deliveredAt: {
+    type: Date,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = mongoose.model("Order", orderSchema);
+
+// ref link
+// https://stackoverflow.com/questions/28997636/should-i-use-schema-types-objectid-or-schema-objectid-when-defining-a-mongoose-s
--- AQ-TNB/models/product.js
+++ AQ-TNB/models/product.js
@@ -1,71 +1,97 @@
 const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema;
 
-const productSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      trim: true,
-      required: true,
-      maxlength: 32,
-      text: true,
-    },
-    slug: {
-      type: String,
-      unique: true,
-      lowercase: true,
-      index: true,
-    },
-    description: {
-      type: String,
-      required: true,
-      maxlength: 2000,
-      text: true,
-    },
-    price: {
-      type: Number,
-      required: true,
-      trim: true,
-      maxlength: 32,
-    },
-    category: {
-      type: ObjectId,
-      ref: "Category",
-    },
-    subs: [
-      {
-        type: ObjectId,
-        ref: "Sub",
-      },
-    ],
-    quantity: Number,
-    sold: {
-      type: Number,
-      default: 0,
-    },
-    images: {
-      type: Array,
-    },
-    shipping: {
-      type: String,
-      enum: ["Yes", "No"],
-    },
-    color: {
-      type: String,
-      enum: ["Black", "Brown", "Silver", "White", "Blue"],
-    },
-    brand: {
-      type: String,
-      enum: ["Apple", "Samsung", "Microsoft", "Lenovo", "ASUS"],
-    },
-    ratings: [
-      {
-        star: Number,
-        postedBy: { type: ObjectId, ref: "User" },
-      },
-    ],
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please provide product name"],
+    trim: true,
+    maxlength: [120, "Product name should not be more than 120 characters"],
   },
-  { timestamps: true }
-);
+  price: {
+    type: Number,
+    required: [true, "please provide product price"],
+    maxlength: [6, "Product price should not be more than 6 digits"],
+  },
+  description: {
+    type: String,
+    required: [true, "please provide product description"],
+  },
+  photos: [
+    {
+      id: {
+        type: String,
+        required: true,
+      },
+      secure_url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [
+      true,
+      "please select category from- short-sleeves, long-sleeves, sweat-shirts, hoodies",
+    ],
+    enum: {
+      values: ["shortsleeves", "longsleeves", "sweatshirt", "hoodies"],
+      message:
+        "please select category ONLY from - short-sleeves, long-sleeves, sweat-shirts and hoodies ",
+    },
+  },
+  categorySpecific: {
+    type: ObjectId,
+    ref: "Category",
+  },
+  //this field was updated in order videos later
+  stock: {
+    type: Number,
+    required: [true, "please add a number in stock"],
+  },
+  brand: {
+    type: String,
+    required: [true, "please add a brand for clothing"],
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  numberOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = mongoose.model("Product", productSchema);
